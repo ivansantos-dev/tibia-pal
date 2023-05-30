d4003df7cfedcf91feed1a6c10ab9a96eeb52691
Ivan Santos <me@ivan-santos.dev>
2023-05-30 07:37:22 -0600

    Initial skeleton app

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,32 @@
+<script lang='ts'>
+	import '../theme.postcss'
+	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	// Most of your app wide CSS should be put in this file
+	import '../app.postcss';
+	import { AppShell, AppBar, Avatar } from '@skeletonlabs/skeleton';
+</script>
+<svelte:head>
+	<title>Tibia Pal</title>
+</svelte:head>
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">Tibia Pal</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<Avatar
+					border="border-4 	border-surface-300-600-token hover:!border-primary-500"
+					cursor="cursor-pointer"
+					width="w-10"
+				/>
+			</svelte:fragment>
+
+		</AppBar>
+	</svelte:fragment>
+	<slot />
+	<svelte:fragment slot="footer">
+		<p>Copyright &copy; 2023</p>
+	</svelte:fragment>
+</AppShell>
