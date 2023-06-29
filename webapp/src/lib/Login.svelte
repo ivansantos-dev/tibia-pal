1312dc8305cebb724da64da9f99875f7f7eba4a9
Ivan Santos <me@ivan-santos.dev>
2023-06-28 22:40:05 -0600

    Fix Login page

--- webapp/src/lib/Login.svelte
+++ webapp/src/lib/Login.svelte
@@ -9,16 +9,19 @@
 	}
 </script>
 
-<div class="card p-4">
-	<label class="label">
-		<span>Email</span>
-		<input class="input" type="text" id="email" bind:value={email} />
-	</label>
+<div class="card max-w-2xl mx-auto p-8 m-4">
+	<h1 class="h1">Sign In</h1>
+	<form>
+		<label class="label">
+			<span>Email</span>
+			<input class="input" type="text" id="email" bind:value={email} />
+		</label>
 
-	<label class="label">
-		<span>Password</span>
-		<input class="input" type="password" id="password" bind:value={password} />
-	</label>
+		<label class="label">
+			<span>Password</span>
+			<input class="input" type="password" id="password" bind:value={password} />
+		</label>
 
-	<button type="button" class="btn variant-filled" on:click={signin}>Login</button>
+		<button type="button" class="btn variant-filled mt-4 end-0" on:click={signin}>Login</button>
+	</form>
 </div>
