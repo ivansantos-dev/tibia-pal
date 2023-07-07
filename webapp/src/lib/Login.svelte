06d30f61c551cda925989ce4eb7522d10171080f
Ivan Santos <me@ivan-santos.dev>
2023-07-07 15:38:29 -0600

    Fix centering for login

--- webapp/src/lib/Login.svelte
+++ webapp/src/lib/Login.svelte
@@ -3,16 +3,18 @@
 	import Icon from '@iconify/svelte';
 </script>
 
-<div class="card p-4">
-	<header class="card-header">
-		<h1 class="h1">Sign In</h1> 
-	</header>
-	<section class="p-4">
-		<button type="button" class="btn variant-filled" on:click={userStore.login}>
-			<span>
-				<Icon icon="devicon:google" />
-			</span>
-			<span>Continue with Google</span>
-		</button>
-	</section>
+<div class="flex justify-center items-center h-screen">
+	<div class="card p-4">
+		<header class="card-header">
+			<h1 class="h1">Sign In</h1> 
+		</header>
+		<section class="p-10">
+			<button type="button" class="btn variant-filled" on:click={userStore.login}>
+				<span>
+					<Icon icon="devicon:google" />
+				</span>
+				<span>Continue with Google</span>
+			</button>
+		</section>
+	</div>
 </div>
